54e7abfd51d858470df46d1ee16772f6b24778ee
EC2 Default User <ec2-user@ip-172-31-40-80.ap-northeast-1.compute.internal>
2023-05-04 08:44:39 +0000

    [add]guestuser

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,4 +1,5 @@
 class Public::CustomersController < ApplicationController
+  before_action :ensure_guest_user, only: [:edit]
   def show
   end
 
@@ -13,4 +14,12 @@ class Public::CustomersController < ApplicationController
 
   def is_deleted
   end
+  
+  private
+  def ensure_guest_user
+    @customer = Customer.find(params[:id])
+    if @customer.name == "guestuser"
+      redirect_to customer_path, notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
+    end
+  end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ devise_for :customers,skip: [:passwords], controllers: {
   sessions: 'public/sessions'
 }
 
+devise_scope :customer do
+    post 'customers/guest_sign_in', to: 'customers/sessions#guest_sign_in'
+end
+
 # 管理者用ログイン
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
