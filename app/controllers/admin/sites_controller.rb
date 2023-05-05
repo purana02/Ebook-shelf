77b6bd3eb6c7f4e7d452996458799c410ed7da00
EC2 Default User <ec2-user@ip-172-31-40-80.ap-northeast-1.compute.internal>
2023-05-05 12:00:25 +0000

    [add]admin-site-genre

--- app/controllers/admin/sites_controller.rb
+++ app/controllers/admin/sites_controller.rb
@@ -1,7 +1,43 @@
 class Admin::SitesController < ApplicationController
   def index
+    @sites = Site.all
+    @site = Site.new
+  end
+
+  def create
+    @site = Site.new(site_params)
+    if @site.save
+      flash[:notice] = "登録に成功しました"
+      redirect_to request.referer
+    else
+      @sites = Site.all
+      render :index
+    end
   end
 
   def edit
+    @site = Site.find(params[:id])
+  end
+
+  def update
+    @site = Site.find(params[:id])
+    if @site.update(site_params)
+      flash[:notice] = "編集に成功しました"
+      redirect_to admin_sites_path
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @site = Site.find(params[:id])
+    @site.destroy
+    flash[:notice] = "削除しました"
+    redirect_to admin_sites_path
+  end  
+
+ private
+  def site_params
+    params.require(:site).permit(:name)
   end
 end
