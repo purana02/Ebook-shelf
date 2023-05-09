8afb69ec0760c492e5160947d628081e4b51b177
EC2 Default User <ec2-user@ip-172-31-40-80.ap-northeast-1.compute.internal>
2023-05-09 13:49:00 +0000

    [add]customer_edit

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -6,7 +6,32 @@ class Admin::CustomersController < ApplicationController
   end
 
   def show
+    @customer = Customer.find(params[:id])
+    @genres = Genre.all
+    @sites = Site.all
+    @reviews = Review.where(customer_id: params[:id]).order(created_at: :desc)
   end
+
   def edit
+    @genres = Genre.all
+    @sites = Site.all
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      flash[:notice] = "変更を保存しました"
+      redirect_to admin_customer_path(@customer)
+    else
+      @genres = Genre.all
+      @sites = Site.all
+      render :edit
+    end
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:nickname, :email, :is_deleted)
   end
 end
