0ea0f305b552fb5409bdbb67f3f159337093a67e
EC2 Default User <ec2-user@ip-172-31-40-80.ap-northeast-1.compute.internal>
2023-05-24 13:08:50 +0000

    delete

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,9 +1,9 @@
 class Site < ApplicationRecord
-  has_many :comics_each_sites, dependent: :destroy
+  has_many :comic_each_sites, dependent: :destroy
   has_many :comics, through: :comic_each_sites
   has_many :having_comics, dependent: :destroy
   has_many :billings, dependent: :destroy
-  
+
   validates :name, presence: true
-  
+
 end
